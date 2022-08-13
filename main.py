4b1591a8cf95ef71aca036262fda33555cb845cd
Sanjin Hadžalić <80854488+SaHaroza014@users.noreply.github.com>
2022-08-13 20:14:35 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
 from tkinter import *
+from tkinter import messagebox
 from password import *
-import time
 
 # main window = Tk()
 window = Tk()
@@ -13,6 +14,7 @@ website_label.grid(row=1, column=0)
 
 # website entry -> appends data to new
 website_entry = Entry()
+website_entry.focus()
 website_entry.config(width=43)
 website_entry.grid(row=1, column=1, columnspan=2, padx=10, pady=10)
 
@@ -23,6 +25,7 @@ username_label.grid(row=2, column=0)
 # username_entry Entry()
 username_entry = Entry(width=43)
 username_entry.grid(row=2, column=1, columnspan=2)
+username_entry.insert(0, 'test.me@testmail.com')
 
 # password Label()
 password_label = Label(text='Password', font=("Calibre", 12, 'normal'))
@@ -51,8 +54,38 @@ def add_to_file():
     website_a = website_entry.get()
     username_a = username_entry.get()
     password_a = password_entry.get()
-    with open('password_saved.txt', 'a') as file:
-        file.write(f'TEST= {website_a} | {username_a} | {password_a} |||\n')
+    new_data = {
+        website_a: {
+            "email": username_a,
+            "password": password_a
+        }
+    }
+
+    if len(website_a) == 0 or len(password_a) == 0:
+        messagebox.showinfo(title="Error!!!", message='You lack some information in your input!')
+    else:
+        if len(password_a) < 12:
+            messagebox.showwarning(title='Password error',
+                                   message='Your password is less than 12 characters. \nPlease try again!\n')
+        else:
+            try:
+                with open('password_saved.json', 'r') as file:
+                    # read old data
+                    data = json.load(file)
+            except FileNotFoundError:
+                with open('password_saved.json', 'w') as file:
+                    json.dump(new_data, file, indent=4)
+            else:
+                # update old data with new data
+                data.update(new_data)
+
+                with open('password_saved.json', 'w') as file:
+                    # save updated data
+                    json.dump(new_data, file, indent=4)
+            finally:
+                website_entry.delete(0, END)
+                # username_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 add_bttn = Button(text='Add', command=add_to_file, width=35)
